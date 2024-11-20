95882dd3960ba1f463fb90634c3f47d6027bd8b5
jl-sky <k849683539@163.com>
2024-11-20 10:37:22 +0800

    fix bug
    Signed-off-by: jl-sky <k849683539@163.com>

--- simplethreadpool/main.cpp
+++ simplethreadpool/main.cpp
@@ -40,23 +40,23 @@ void example() {
   // 提交乘法操作，总共30个
   for (int i = 1; i < 3; ++i) {
     for (int j = 1; j < 10; ++j) {
-      pool.submit(multiply, i, j);
+      pool.Submit(multiply, i, j);
     }
   }
 
   // 使用ref传递的输出参数提交函数
   int output_ref;
-  auto future1 = pool.submit(multiply_output, std::ref(output_ref), 5, 6);
+  auto future1 = pool.Submit(multiply_output, std::ref(output_ref), 5, 6);
   // 等待乘法输出完成
   future1.get();
   std::cout << "Last operation result is equals to " << output_ref << std::endl;
   // 使用return参数提交函数
-  auto future2 = pool.submit(multiply_return, 5, 3);
+  auto future2 = pool.Submit(multiply_return, 5, 3);
   // 等待乘法输出完成
   int res = future2.get();
   std::cout << "Last operation result is equals to " << res << std::endl;
   //关闭线程池
-  // pool.shutdown();
+  // pool.Shutdown();
 }
 
 int main()
